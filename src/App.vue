e80c6c1b59c265b0775c4d08215e1961f48beefe
m-ConnorBoyle <m.connorboyle@gmail.com>
2024-09-08 16:03:28 +1000

    refactor

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import PrivayPolicy from './components/PrivacyPolicy.vue'
+</script>
+
+<template>
+  <h1>
+Taper Offset Spacing Calculator Privacy Policy
+  </h1>
+  <PrivayPolicy />
+</template>
